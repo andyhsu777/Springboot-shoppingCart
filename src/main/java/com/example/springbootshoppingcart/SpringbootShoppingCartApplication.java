421481524eb2a762160094f3576234c4c953cc34
andyhsu777 <hantinghsuu@gmail.com>
2023-09-13 00:46:30 +0800

    initialize project

---
+++ src/main/java/com/example/springbootshoppingcart/SpringbootShoppingCartApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootshoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootShoppingCartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootShoppingCartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootshoppingcart/SpringbootShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootshoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootShoppingCartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
