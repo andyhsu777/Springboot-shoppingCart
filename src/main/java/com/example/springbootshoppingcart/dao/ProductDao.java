98c68382f90d61802abfbe26e27e28de665b707f
andyhsu777 <hello@123.tw>
2023-09-30 00:13:13 +0800

    implement query products function

--- src/main/java/com/example/springbootshoppingcart/dao/ProductDao.java
+++ src/main/java/com/example/springbootshoppingcart/dao/ProductDao.java
@@ -3,9 +3,13 @@ package com.example.springbootshoppingcart.dao;
 import com.example.springbootshoppingcart.dto.ProductRequest;
 import com.example.springbootshoppingcart.model.Product;
 
+import java.util.List;
 
 
 public interface ProductDao {
+
+    List<Product> getProducts();
+
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
--- src/main/java/com/example/springbootshoppingcart/service/ProductService.java
+++ src/main/java/com/example/springbootshoppingcart/service/ProductService.java
@@ -3,7 +3,12 @@ package com.example.springbootshoppingcart.service;
 import com.example.springbootshoppingcart.dto.ProductRequest;
 import com.example.springbootshoppingcart.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
+
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
